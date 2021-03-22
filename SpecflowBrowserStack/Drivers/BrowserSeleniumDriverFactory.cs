8fc1be548413af20398c75905660389d3a99e930
mayankmaurya21 <mayank.m@browserstack.com>
2021-03-22 12:27:03 +0530

    first commit

---
+++ SpecflowBrowserStack/Drivers/BrowserSeleniumDriverFactory.cs
@@ -0,0 +1,230 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using BrowserStack;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+using TechTalk.SpecRun;
+
+namespace SpecflowBrowserStack.Drivers
+{
+    public class BrowserSeleniumDriverFactory
+    {
+        private readonly ConfigurationDriver _configurationDriver;
+        private readonly TestRunContext _testRunContext;
+        string remoteUrl = "";
+        private FeatureContext _featureContext;
+        private ScenarioContext _scenarioContext;
+        private static ExtentReports extent;
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentHtmlReporter htmlReporter;
+
+        public BrowserSeleniumDriverFactory(ConfigurationDriver configurationDriver, TestRunContext testRunContext, FeatureContext featureContext, ScenarioContext scenarioContext)
+        {
+            _configurationDriver = configurationDriver;
+            _testRunContext = testRunContext;
+            _featureContext = featureContext;
+            _scenarioContext = scenarioContext;
+        }
+
+        public IWebDriver GetForBrowser(int browserId)
+        {
+            // sets remote URL
+            string username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+            if (username == null || username == "")
+            {
+                username = _configurationDriver.Username;
+            }
+            string access_key = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+            if (access_key == null || access_key == "")
+            {
+                access_key = _configurationDriver.AccessKey;
+            }
+            Environment.SetEnvironmentVariable("TEST_INFRA", "");
+            string infra = Environment.GetEnvironmentVariable("TEST_INFRA");
+
+            if (infra == "DOCKER")
+            {
+                remoteUrl = _configurationDriver.SeleniumBaseUrl + "/wd/hub/";
+            }
+            else
+            {
+                remoteUrl = "https://" + username + ":" + access_key + "@" + _configurationDriver.SeleniumBaseUrl + "/wd/hub/";
+            }
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            // Set common capabilities like "browserstack.local", project, name, session
+            foreach (var tuple in _configurationDriver.CommonCapabilities)
+            {
+                caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString());
+            }
+            if (browserId == 0)
+            {
+                var specificCap = _configurationDriver.Single.ToList<IConfigurationSection>()[browserId];
+                foreach (var tuple in specificCap.GetChildren().AsEnumerable())
+                {
+                    caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString());
+                    if (tuple.Key.ToString() == "name")
+                    {
+                        caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString() + " " + _featureContext.FeatureInfo.Title);
+                    }
+                    if (infra == "ON_PREM" && tuple.Key.ToString() == "browser")
+                    {
+                        return OnPremDrivers(tuple.Value.ToString());
+                    }
+                }
+                return new RemoteWebDriver(new Uri(remoteUrl), caps);
+            }
+            else if (browserId == 1)
+            {
+                var specificCap = _configurationDriver.Local.ToList<IConfigurationSection>()[browserId - 1];
+                foreach (var tuple in specificCap.GetChildren().AsEnumerable())
+                {
+                    caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString());
+                    if (tuple.Key.ToString() == "name")
+                    {
+                        caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString() + " " + _featureContext.FeatureInfo.Title);
+                    }
+                    if (infra == "ON_PREM" && tuple.Key.ToString() == "browser")
+                    {
+                        return OnPremDrivers(tuple.Value.ToString());
+                    }
+                }
+                return new RemoteWebDriver(new Uri(remoteUrl), caps);
+            }
+
+            else if (browserId > 2)
+            {
+                // Set session specific capability
+                var specificCaps = _configurationDriver.Parallel.ToList<IConfigurationSection>()[browserId - 3];
+                foreach (var tuple in specificCaps.GetChildren().AsEnumerable())
+                {
+                    caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString());
+                    if (tuple.Key.ToString() == "name")
+                    {
+                        caps.SetCapability(tuple.Key.ToString(), tuple.Value.ToString() + " " + _featureContext.FeatureInfo.Title);
+                    }
+                    if (infra == "ON_PREM" && tuple.Key.ToString() == "browser")
+                    {
+                        return OnPremDrivers(tuple.Value.ToString());
+                    }
+                }
+                return new RemoteWebDriver(new Uri(remoteUrl), caps);
+            }
+
+            else { return null; }
+            // return null;
+        }
+
+        public AndroidDriver<AndroidElement> GetForMobileBrowser(int browserId)
+        {
+            AppiumOptions capability = new AppiumOptions();
+            // sets remote URL
+            string username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+            if (username == null || username == "")
+            {
+                username = _configurationDriver.Username;
+            }
+            string access_key = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+            if (access_key == null || access_key == "")
+            {
+                access_key = _configurationDriver.AccessKey;
+            }
+            string remoteUrl = "https://";
+            if (username != null && access_key != null)
+            {
+                remoteUrl += username + ":" + access_key + "@";
+            }
+            remoteUrl += _configurationDriver.SeleniumBaseUrl + "/wd/hub";
+            if (browserId == 2)
+            {
+                // Set session specific capability
+                var specificCaps = _configurationDriver.Mobile.ToList<IConfigurationSection>()[browserId - 2];
+                foreach (var tuple in specificCaps.GetChildren().AsEnumerable())
+                {
+                    capability.AddAdditionalCapability(tuple.Key.ToString(), tuple.Value.ToString());
+                }
+                return new AndroidDriver<AndroidElement>(new Uri(remoteUrl), capability);
+            }
+            else { return null; }
+        }
+
+        public Local GetLocal(int browserIndex)
+        {
+            if (browserIndex < 2)
+            {
+                var specificCap = _configurationDriver.Local.ToList<IConfigurationSection>()[0];
+
+                foreach (var tuple in specificCap.GetChildren().AsEnumerable())
+                {
+                    if (tuple.Key.ToString() == "browserstack.local")
+                    {
+                        Local _local = new Local();
+                        List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
+                        new KeyValuePair<string, string>("key", _configurationDriver.AccessKey)
+                    };
+                        _local.start(bsLocalArgs);
+                        return _local;
+                    }
+                }
+                return null;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IWebDriver OnPremDrivers(String browser)
+        {
+            if (browser == "firefox")
+                return new FirefoxDriver(@"..\..\browserstack-examples-specflow\SpecflowBrowserStack\Drivers\OnPremDriver");
+            else if (browser == "chrome")
+                return new ChromeDriver(@"..\..\browserstack-examples-specflow\SpecflowBrowserStack\Drivers\OnPremDriver");
+            else if (browser == "internet explorer")
+                return new InternetExplorerDriver(@"..\..\browserstack-examples-specflow\SpecflowBrowserStack\Drivers\OnPremDriver");
+            else
+                return null;
+        }
+
+        [BeforeTestRun]
+        public static void initializereport()
+        {
+            htmlReporter = new ExtentHtmlReporter(@"..\..\browserstack-examples-specflow\SpecflowBrowserStack\Extendreport.html");
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [AfterTestRun]
+        public static void teardownReport()
+        {
+            extent.Flush();
+        }
+        [BeforeFeature]
+        public void BeforeFeature()
+        {
+            //Create dynamic feature name
+            featureName = extent.CreateTest<Feature>(_featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public void Initialize(ScenarioContext scenarioContext)
+        {
+            //Create dynamic scenario name
+            scenario = featureName.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
+        }
+    }
+}
